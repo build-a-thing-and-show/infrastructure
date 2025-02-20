7b7b84716795f46806abe1ed0c4a16c7863f1495
Reza <rzgholizadeh@gmail.com>
2025-02-19 18:13:48 -0800

    Address typo

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "batash-terraform-infrastructure"
-    key            = "ec2-instance/terraform.tfstate" # we can rename this and even add more keys as the infra gets more complex
+    key            = "ec2-instance/terraform.tfstate" # We can rename this and even add more keys as the infra gets more complex
     region         = "us-east-1"
     encrypt        = true
   }
