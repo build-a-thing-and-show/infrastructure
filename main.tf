843f11214eac412720a3254aa944223bee5ba4f6
Reza <rzgholizadeh@gmail.com>
2025-02-20 19:26:39 -0800

    Add EC2 instance

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ resource "aws_ssm_parameter" "foo" {
     type = "String"
     value = "bar"
 }
+
+resource "aws_instance" "tr_ec2" {
+    ami = "ami-04b4f1a9cf54c11d0"
+    instance_type = "t2.micro"
+    ebs_block_device {
+      device_name = "tr_ec2_ssd"
+      volume_size = 8
+      volume_type = "gp3"
+    }
+}
