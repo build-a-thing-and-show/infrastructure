2932f17e5515d1d17e6d5bab51c74fb0404b284c
Reza <rzgholizadeh@gmail.com>
2025-02-20 19:30:31 -0800

    Add key name and ip configuration

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ resource "aws_ssm_parameter" "foo" {
 resource "aws_instance" "tr_ec2" {
     ami = "ami-04b4f1a9cf54c11d0"
     instance_type = "t2.micro"
+    key_name = "batash00"
+    associate_public_ip_address = true
     ebs_block_device {
       device_name = "tr_ec2_ssd"
       volume_size = 8
