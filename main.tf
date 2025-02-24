240a27805d8097e52595a3e6d11de72a36b6d6cc
Reza <rzgholizadeh@gmail.com>
2025-02-23 21:13:44 -0800

    Use terraform example

--- main.tf
+++ main.tf
@@ -18,9 +18,16 @@ resource "aws_instance" "tr_ec2" {
 }
 */
 
-resource "aws_eks_cluster" "tf_cluster" {
-  name = "name_for_tf_cluster"
-  role_arn = aws_iam_role.cluster.arn
+resource "aws_eks_cluster" "example" {
+  name = "example"
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  role_arn = aws_iam_role.example.arn
+  version  = "1.31"
+
   vpc_config {
     subnet_ids = [
       aws_subnet.az1.id,
@@ -28,6 +35,13 @@ resource "aws_eks_cluster" "tf_cluster" {
       aws_subnet.az3.id,
     ]
   }
+
+  # Ensure that IAM Role permissions are created before and deleted
+  # after EKS Cluster handling. Otherwise, EKS will not be able to
+  # properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
 }
 
 resource "aws_iam_role" "cluster" {
@@ -47,4 +61,9 @@ resource "aws_iam_role" "cluster" {
       },
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
 }
