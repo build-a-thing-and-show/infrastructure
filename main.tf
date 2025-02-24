25983d00f671ec86a72a32ead8a3a514c1a33ef4
Reza <rzgholizadeh@gmail.com>
2025-02-23 21:05:48 -0800

    Add eks description

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_ssm_parameter" "foo" {
     name = "foo"
     type = "String"
@@ -14,4 +15,36 @@ resource "aws_instance" "tr_ec2" {
       volume_size = 8
       volume_type = "gp3"
     }
+}
+*/
+
+resource "aws_eks_cluster" "tf_cluster" {
+  name = "name_for_tf_cluster"
+  role_arn = aws_iam_role.cluster.arn
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.az1.id,
+      aws_subnet.az2.id,
+      aws_subnet.az3.id,
+    ]
+  }
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-example"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
