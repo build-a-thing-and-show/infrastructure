a3d327d68297c083d4e50fc4fbf5aaa31bd5edb5
Reza <rzgholizadeh@gmail.com>
2025-02-23 21:27:15 -0800

    Change to valid availability zone

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 resource "aws_subnet" "az1" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "eks-private-subnet-2"
@@ -81,7 +81,7 @@ resource "aws_subnet" "az1" {
 resource "aws_subnet" "az2" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
