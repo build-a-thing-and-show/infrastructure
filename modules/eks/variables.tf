4a9142bffd42c01ed590e49d08aee099c22b0734
Reza <rzgholizadeh@gmail.com>
2025-02-24 11:31:22 -0800

    Move eks specification to modules

---
+++ modules/eks/variables.tf
@@ -0,0 +1,17 @@
+variable "eks_cluster_name" {
+  type    = string
+  default = "batas_eks_cluster"
+}
+
+variable "eks_iam_role_arn" {
+  type = string
+}
+
+variable "eks_version" {
+  type    = string
+  default = "1.31"
+}
+
+variable "eks_subnet_ids" {
+  type = list(string)
+}
