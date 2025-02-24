217e0e65b7876682f271dbdf7f04d56eaba56562
Reza <rzgholizadeh@gmail.com>
2025-02-24 11:42:47 -0800

    Add prod environment specs

---
+++ environments/production/main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source = "../../modules/vpc"
+}
+
+module "iam" {
+  source = "../../modules/iam"
+}
+
+module "eks" {
+  source             = "../../modules/eks"
+  eks_iam_role_arn   = module.iam.iam_role_arn
+  eks_subnet_ids = [
+    module.vpc.subnet_private_id,
+    module.vpc.subnet_public_id,
+  ]
+}
