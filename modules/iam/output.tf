b0c1330ed7bca1810848fa40c8deab6da7b1dcfd
Reza <rzgholizadeh@gmail.com>
2025-03-29 15:32:37 -0700

    Correct type for node group iam roles

--- modules/iam/output.tf
+++ modules/iam/output.tf
@@ -7,9 +7,9 @@ output "iam_role_name" {
 }
 
 output "eks_node_group_iam_role_arn" {
-  value = aws_iam_role.eks_cluster.arn
+  value = aws_iam_role.eks_node_group.arn
 }
 
 output "eks_node_group_iam_role_name" {
-  value = aws_iam_role.eks_cluster.name
+  value = aws_iam_role.eks_node_group.name
 }
