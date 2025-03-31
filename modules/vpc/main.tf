a802d204f69691437737856913d66d6f1428930a
Reza <rzgholizadeh@gmail.com>
2025-03-31 12:44:27 -0700

    Add internet gateway access to public subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -19,6 +19,31 @@ resource "aws_subnet" "private" {
   }
 }
 */
+
+// Create internet gateway for the pbulic subents
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.batas_vpc.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+// Create the route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.batas_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+
 //--> for experimental reason, only use public subnet at this moment. We will change this once we add the API Gateway, ALB and NAT Gateway 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.batas_vpc.id
@@ -43,4 +68,16 @@ resource "aws_subnet" "public" {
     "kubernetes.io/role/elb" = "1"
     "kubernetes.io/cluster/eks_cluster" = "shared"   // Instead of hardcoding the cluster name, we should use ${var.cluster_name}
   }
+}
+
+
+// Create the associations between subnets and the public route table to internet gateway
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
